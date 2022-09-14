2f74de202d37410cfaac061a035d2c39cba8cc3d
David Lambert <david.lambert@my.utsa.edu>
2022-09-14 13:38:00 -0500

    added mocha test

--- index.test.js
+++ index.test.js
@@ -1,37 +1,31 @@
-
+const assert = require('assert')
 const { forEach, map } = require("./index");
 
-const test = (desc, fn) => {
+//handwritten test 
+/*const test = (desc, fn) => {
   console.log("----", desc);
   try { //allows the prog to continue running if error
     fn();
   } catch (err) {
     console.log(err.message);
   }
-};
-
-test("The forEach func", () => {
+}; */
+//it is same as my handwritten test func
+it("The forEach func", () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => {
     sum = sum + value;
   });
-  if (sum !== 6) {
-    throw new Error("Expected sum to equal 6");
-  }
+  assert.strictEqual(sum, 6, "Expected sum to equal 6"); //optional 3rd arg string for error message to console
+
 });
 
-test("The map func", () => {
+it("The map func", () => {
   //map returns new array with results
   const result = map([1, 2, 3], (value) => {
     return value * 2;
   });
-  if (result[0] !== 2) {
-    throw new Error(`Expected 2 but found ${result[0]}`);
-  }
-  if (result[1] !== 4) {
-    throw new Error(`Expected 4 but found ${result[1]}`);
-  }
-  if (result[2] !== 6) {
-    throw new Error(`Expected 6 but found ${result[2]}`);
-  }
+  assert.strictEqual(result[0], 2)
+  assert.strictEqual(result[1], 4);
+  assert.strictEqual(result[2], 6);
 });
